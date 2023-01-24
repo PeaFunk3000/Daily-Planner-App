ea543827c5be501cab22ac5fa2a9303343be3b26
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-24 18:16:16 +0000

    moment.js hour and day variables defined

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+var theDay = moment().format("dddd, D MMMM YYYY");
+var theHour = moment().format("kk");
+
+console.log(theDay + theHour);
