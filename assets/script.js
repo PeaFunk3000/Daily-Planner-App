3599263b4262869a4f16ac4b4933fcdb8479cbcc
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-24 18:21:38 +0000

    currentDay in header of page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 console.log("Hello World!");
 
+// moment.js day and hour vars
 var theDay = moment().format("dddd, D MMMM YYYY");
 var theHour = moment().format("kk");
 
 console.log(theDay + theHour);
+
+// jquery DOM selectors
+var saveBtn = $(".saveBtn");
+var taskText = $(".description");
+var timeDivs = $(".time-div");
+var clearBtn = $(".clearBtn");
+var currentDay = $("#currentDay");
+
+// set currentDay in header p tag
+currentDay.text(theDay);
+
+
