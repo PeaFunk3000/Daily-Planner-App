5cfce2675869868fe7a5927c1f6670b8210cdfad
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-01-24 19:09:48 +0000

    setInterval function to update day every second, saveBtn.click added to save to local

--- assets/script.js
+++ assets/script.js
@@ -16,4 +16,17 @@ var currentDay = $("#currentDay");
 // set currentDay in header p tag
 currentDay.text(theDay);
 
+// setInterval to update theDay
+setInterval(function () {
+    var clockTime = moment();
+    $("#currentDay").text(clockTime.format("dddd, D MMMM YYYY"))
+}, 1000);
 
+// click function for save buttons
+saveBtn.click(function () {
+    var task = $(this).siblings(".description").val();
+    var taskTime = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(taskTime, task);
+});
+
+console.log(localStorage);
